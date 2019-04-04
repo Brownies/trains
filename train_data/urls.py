c44248e18413e5c7c4fcbe192cf9b493ab2409fb
Lassi Heikkinen <lassi.heikkinen@aalto.fi>
2019-04-04 21:30:27 +0300

    work on user system and templates

--- train_data/urls.py
+++ train_data/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('trains/<int:train_id>/location/', views.insert_train, name='insert_train'),
-    path('display_trains/', views.display_trains, name='display_trains')
+    path('display_trains/', views.display_trains, name='display_trains'),
+    path('', views.index, name='index'),
+    path('login/', auth_views.LoginView.as_view(template_name='train_data/login.html')),
+    # path('logout/', logout, {'next_page': 'index'}, name='logout')
 
 ]
--- train_data/views.py
+++ train_data/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.template import loader
@@ -16,4 +18,28 @@ def display_trains(request):
 
 
 def insert_train(request, train_id):
-    return HttpResponse("hello")
+    if request.methold != 'PUT':
+        return HttpResponseBadRequest("Invalid request method")
+    elif request.content_type != 'application/json':
+        return HttpResponseBadRequest("Request content type is not application/json")
+
+
+def index(request):
+    if not request.user.is_authenticated:
+        template = loader.get_template('train_data/login_or_register.html')
+        return HttpResponse(template.render({}, request))
+
+    template = loader.get_template('train_data/index.html')
+    return HttpResponse(template.render({}, request))
+
+
+def register(request):
+    if request.method == 'GET':
+        template = loader.get_template('train_data/register.html')
+        return HttpResponse(template.render({}, request))
+
+    elif request.method == 'POST':
+        pass
+
+    else:
+        return HttpResponseBadRequest()
